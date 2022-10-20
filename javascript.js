79da62488d6e94174fa532cbe4923ad1f992604a
Andrew-Solodovnik <116022831+Andrew-Solodovnik@users.noreply.github.com>
2022-10-20 20:46:30 +0200

    time and date

--- javascript.js
+++ javascript.js
@@ -36,11 +36,134 @@ console.log(number);
 
 }
 
-ClockBtn.addEventListener("click", footerClockOpen)
+ClockBtn.addEventListener("click", footerClockOpen);
+
+
+
+// const isAdmin = true;
+
+
+// if(isAdmin === true) {
+//     console.log ("zalogowano jako admin")
+// }
+
+// else {
+//     console.log("Brak dostepu")
+// }
+
+// console.log (isAdmin);
 
 
 
 
+
+
+// const user ="jan"
+
+// if(user === "janek") {
+//     console.log ("zalogowano jako admin")
+// }
+
+// else {
+//     console.log("Brak dostepu")
+// }
+
+// console.log (isAdmin);
+
+
+// const isAdmin = true;
+
+
+// if(isAdmin === true) {
+//     console.log ("zalogowano jako admin")
+// }
+
+// else {
+//     console.log("Brak dostepu")
+// }
+
+// console.log (isAdmin);
+
+
+
+
+// const num = "45"
+
+// if(num == 45)
+// {
+//     console.log("liczba sie zgadza")
+// }
+
+
+// const num = 11;
+
+// if(num > 10)
+// {
+//     console.log("liczba wieksza od 10")
+// }
+// else {
+//     console.log("liczba mniejsza od 10")
+// }
+
+// const loginBtn = document.getElementById("Login");
+// const loginField = document.getElementById("Field");
+// const passwordField = document.getElementById("Password");
+
+
+// function login () {
+//     if(loginField.value === "admin" && passwordField.value === "haslo123") {
+//         console.log("Witamy w panelu")
+//     }
+
+//     else if(loginField.value === "godmode") {
+//         console.log("odblokovano dostep")
+//     }
+    
+//     else {
+//         console.log("Bledne dane");
+//     }
+// }
+
+// loginBtn.addEventListener("click", login);
+
+
+
+
+
+const greeting = document.getElementById("Greeting");
+
+function showGreeting () {
+
+const hour = new Date ().getHours();
+if(hour >= 6 && hour < 1) {
+    greeting.textContent = "Good Morning";
+}
+
+else if(hour >= 1 && hour < 18) {
+
+    greeting.textContent = "Good Afternoon";
+}
+
+else if(hour >= 18 && hour < 24) {
+
+    greeting.textContent = "Good Evening";
+}
+
+else {
+    greeting.textContent = "Good Hight"
+}
+
+}
+showGreeting();
+
+
+
+// ================== DAY ================== //
+
+const currentDay = document.getElementById("Day")
+const day = new Date().toLocaleString('en-us', {weekday:'long'});
+currentDay.textContent = day
+console.log(day);
 
 
 
